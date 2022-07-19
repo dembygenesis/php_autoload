8434409bd7587cb5c2b9d74a6461ea3abe69393a
Demby <dmcchy@gmail.com>
2022-07-19 18:58:12 +0800

    Some progress

--- index.php
+++ index.php
@@ -4,5 +4,18 @@ require_once 'app/init.php';
 use Codechief\Filters\Filter as Filter;
 use Codechief\Model\User as User;
 
-$filter = new Filter();
-echo $filter->GetDataFromUserClass(new User());
+/*$filter = new Filter();
+echo $filter->GetDataFromUserClass(new User());*/
+
+// Try here
+// $grpcService = new \Codechief\GRPC\ChatServiceClient("localhost:9000", []);
+$credentials = \Grpc\ChannelCredentials::createInsecure();
+
+$grpcService = new \Codechief\GRPC\ChatServiceClient(
+    "localhost:9000",
+    ['credentials' => $credentials]
+);
+
+
+var_dump($grpcService);
+
