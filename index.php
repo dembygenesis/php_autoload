55e46d3d2283223d6f67f57a1d72c749dc7195f3
Demby <dmcchy@gmail.com>
2022-07-19 19:11:11 +0800

    Enclosed in try-catch

--- index.php
+++ index.php
@@ -9,13 +9,27 @@ echo $filter->GetDataFromUserClass(new User());*/
 
 // Try here
 // $grpcService = new \Codechief\GRPC\ChatServiceClient("localhost:9000", []);
-$credentials = \Grpc\ChannelCredentials::createInsecure();
+try {
+    $credentials = \Grpc\ChannelCredentials::createInsecure();
 
-$grpcService = new \Codechief\GRPC\ChatServiceClient(
-    "localhost:9000",
-    ['credentials' => $credentials]
-);
+    $grpcService = new \Codechief\GRPC\ChatServiceClient(
+        "localhost:9000",
+        ['credentials' => $credentials]
+    );
 
+    /**
+     * (\Codechief\GRPC\Message $argument,
+    $metadata = [], $options = [])
+     */
 
-var_dump($grpcService);
+    $message = new \Codechief\GRPC\Message(['body' => "Hello, Go!"]);
+
+    $response = $grpcService->SayHello($message)->wait();
+
+    var_dump($response);
+} catch (Exception $e) {
+    var_dump($e);
+    die("End exception");
+}
+
 
